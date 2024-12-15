535f68a0f622688937ec13acd484d5e8240d6f20
chenlei4 <chenlei4@yy.com>
2024-12-15 16:18:49 +0800

    优化

--- src/main/java/com/learn/strategysvc/strategy/LogisticsStrategyFactory.java
+++ src/main/java/com/learn/strategysvc/strategy/LogisticsStrategyFactory.java
@@ -1,8 +1,11 @@
 package com.learn.strategysvc.strategy;
 
 import com.learn.strategysvc.common.constant.LogisticsType;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -12,11 +15,20 @@ import java.util.concurrent.ConcurrentHashMap;
  * @create 2024-12-15 12:32
  */
 @Component
-public class LogisticsStrategyFactory {
+public class LogisticsStrategyFactory implements InitializingBean {
     private final Map<LogisticsType, LogisticsStrategy> logisticsStrategyMap = new ConcurrentHashMap<>();
 
+    @Resource
+    private ApplicationContext applicationContext;
+    @Override
+    public void afterPropertiesSet() {
+        Map<String, LogisticsStrategy> map = applicationContext.getBeansOfType(LogisticsStrategy.class);
+        map.forEach((k, v) -> logisticsStrategyMap.put(v.getLogistics(), v));
+    }
 
     public LogisticsStrategy getLogisticsStrategy(LogisticsType logisticsType) {
         return logisticsStrategyMap.get(logisticsType);
     }
+
+
 }
